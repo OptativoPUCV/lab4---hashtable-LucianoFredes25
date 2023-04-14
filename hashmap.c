1fa148da14744718e845e32862f23b9c3cddea58
Replit user <>
2023-04-14 01:39:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -42,6 +42,7 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value) {
   int aux = hash(key,map->capacity);
   map->buckets[aux] = createPair(key, value);
+  map->size++;
 }
 
 void enlarge(HashMap * map) {
