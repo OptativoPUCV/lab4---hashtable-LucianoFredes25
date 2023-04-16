b27730f123e9e2d8d810e27abdc25c61b6b21954
Replit user <>
2023-04-16 01:42:23 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -60,7 +60,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 void enlarge(HashMap * map) {
   enlarge_called = 1; //no borrar (testing purposes)
   
-  HashMap * mapa = createMap(map->capacity);
+  /*HashMap * mapa = createMap(map->capacity);
   mapa->buckets = map->buckets;
   mapa->size = map->size;
   Pair * aux = firstMap(mapa);
@@ -72,7 +72,16 @@ void enlarge(HashMap * map) {
       break;
     insertMap(map, aux->key, aux->value);
     aux=nextMap(mapa);
-  }
+  }*
+  
+  /*Pair ** aux = map->buckets;
+  map->capacity *= 2;
+  HashMap * mapa = createMap(map->capacity);
+  map->buckets = mapa->buckets;
+  map->size = 0;
+  for(size_t i = 0 ; i < map->capacity / 2 ; i++){
+    insertMap(map, aux[i]->key, aux[i]->value);
+  }*/
 }
 
 HashMap * createMap(long capacity) {
